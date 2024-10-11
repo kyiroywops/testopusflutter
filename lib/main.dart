56778542b0aec045a979f6467cd4e5ae37dbbc26
Kylian Leroy <aleexisazul@gmail.com>
2024-10-11 00:01:41 -0300

    no es compatible

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:ffmpeg_kit_flutter/return_code.dart';
 import 'package:flutter/material.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:just_audio/just_audio.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Reproducir Opus')),
+        appBar: AppBar(title: Text('Reproducir Opus convertido')),
         body: AudioPlayerWidget(),
       ),
     );
@@ -22,21 +26,46 @@ class AudioPlayerWidget extends StatefulWidget {
 
 class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
   final player = AudioPlayer();
+  String? convertedFilePath;
 
   @override
   void initState() {
     super.initState();
-    // Carga y reproduce el archivo Opus desde la URL proporcionada
-    player.setUrl(
-        "https://firebasestorage.googleapis.com/v0/b/meditapp-b0787.appspot.com/o/1.opus?alt=media&token=65002a5b-f213-47c0-b133-9f468f1d3ee4");
+    _convertOpusToMp3();
+  }
+
+  Future<void> _convertOpusToMp3() async {
+    // Define la URL del archivo Opus a convertir
+    String opusUrl =
+        "https://firebasestorage.googleapis.com/v0/b/meditapp-b0787.appspot.com/o/1.opus?alt=media&token=65002a5b-f213-47c0-b133-9f468f1d3ee4";
+
+    // Obtener el directorio temporal para guardar el archivo convertido
+    final directory = await getTemporaryDirectory();
+    convertedFilePath = '${directory.path}/output.mp3';
+
+    // Ejecuta la conversión con FFmpeg Kit
+    await FFmpegKit.execute(
+        '-i "$opusUrl" "${convertedFilePath!}"').then((session) async {
+      final returnCode = await session.getReturnCode();
+      if (ReturnCode.isSuccess(returnCode)) {
+        print("Conversión exitosa: $convertedFilePath");
+        await player.setFilePath(convertedFilePath!);
+      } else {
+        print("Falló la conversión.");
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: ElevatedButton(
-        onPressed: () => player.play(),
-        child: Text("Reproducir Opus"),
+        onPressed: () {
+          if (convertedFilePath != null) {
+            player.play();
+          }
+        },
+        child: Text("Reproducir MP3 convertido"),
       ),
     );
   }
